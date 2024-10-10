2517741c1fcc553af84832f6379e508981a867a4
Pâris Gougne <paris.gougne@etu.univ-grenoble-alpes.fr>
2024-10-10 09:36:32 +0200

    react commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Pâris + Seyma</h1>
+      <h1>Pâris + Seyma + Noé + Joran + Antonin</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
