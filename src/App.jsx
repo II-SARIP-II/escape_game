91c21e7c379a6b5fdae5c636dc969ab37131c32f
Pâris Gougne <paris.gougne@etu.univ-grenoble-alpes.fr>
2024-10-10 14:41:45 +0200

    only mobile access

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,37 @@ import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Navbar from "./pages/Navbar.jsx";
+import { isMobile } from 'react-device-detect';
+
+function useMobileOverride() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('mobile') === 'true';
+}
 
 function App() {
+    const isMobileOverride = useMobileOverride();
+
+    const renderContent = () => {
+        if (isMobile || isMobileOverride) {
+            return (
+                <div>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </div>
+            );
+        }
+        return <div>Ce site est accessible uniquement sur mobile.</div>;
+    };
+
     return (
         <div>
-            <Navbar/>
-            <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/notfound" element={<NotFound />} />
-            </Routes>
+            {renderContent()}
         </div>
     );
 }
-
 
 export default App;
